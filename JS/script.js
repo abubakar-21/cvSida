81ded6d3863e983e87fd7aefd223727c1ad5ec71
Abubakar Abdulkader <abubakarabdulkader21@gmail.com>
2022-10-17 09:39:04 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,19 +4,6 @@ icon.addEventListener("click", ()=> {
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         icon.name = "sunny"
-        // icon.style.fill = "white" // rad 85 i css style filen
     }
     else{
         icon.name = "moon"
-        // icon.style.fill = "black" // rad 85 i css style filen
-    }
-})
-
-// let navItem = document.getElementsByClassName("navItem")[0];
-// console.log(navItem);
-
-// navItem.addEventListener("click", ()=> {
-//     console.log("clicked!");
-//     document.body.classList.toggle("active-nav");
-//     navItem.style.color = "red";
-// })
